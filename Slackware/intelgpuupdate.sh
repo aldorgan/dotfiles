b761faceaed6a9316343a91a72a3fb044b649333
Ditt Namn <du@example.com>
2019-09-26 18:42:29 +0200

    orginazed a bit

--- Slackware/intelgpuupdate.sh
+++
@@ -1,7 +0,0 @@
-wget -r -nH --no-parent --reject="index.html*" --cut-dirs=5 https://slackbuilds.org/mirror/slackware/slackware64-14.2/source/x/x11/
-cd x11/src/driver
-rm xf86-video-intel-git_20160601_b617f80.tar.xz
-sh ../get-xf86-video-intel.sh
-cd ../..
-sh x11.SlackBuild driver xf86-video-intel
-upgradepkg /tmp/xf86-video-intel*.txz
