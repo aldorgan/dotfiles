43fb5a8f63cd882ed5112d094242f6e63cafb2b6
crash <crash@crashs-MacBook-Air.local>
2019-10-22 13:07:13 +0200

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt install firmware-linux tlp thermald network-manager-gnome xorg lightdm lightdm-gtk-greeter evince file-roller nautilus libreoffice libreoffice-gtk3 mpv vlc x264 x265 pulseaudio python3-pip gvfs gvfs-fuse
+
+
