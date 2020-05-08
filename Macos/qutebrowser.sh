994f86187cc84f070e8816ca41d552f86755fd41
crash <crash@MacBook-Air-som-tillhor-crash.local>
2020-05-08 20:50:15 +0200

    added some files

---
+++ Macos/qutebrowser.sh
@@ -0,0 +1,1 @@
+/Applications/qutebrowser.app/Contents/MacOS/qutebrowser --qt-flag ignore-gpu-blacklist --qt-flag enable-gpu-rasterization --qt-flag enable-native-gpu-memory-buffers --qt-flag num-raster-threads=4
