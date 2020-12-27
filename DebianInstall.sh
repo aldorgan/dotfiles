94bb2bcb02120f64dec3dc7b5c30e6c87aba9c5e
Crash <danielolsson@linuxmail.org>
2020-12-27 20:59:42 +0100

    Remove files

--- DebianInstall.sh
+++
@@ -1,5 +0,0 @@
-sudo apt update
-sudo apt install firmware-linux tlp thermald gvfs-fuse -y
-sudo apt -t buster-backports install tlp -y
-sudo apt install git mpv vlc x264 x265 python3-pip -y && sudo apt remove youtube-dl -y && sudo apt autoremove -y && sudo apt install ffmpeg -y && sudo pip3 install youtube-dl
-git clone https://github.com/aldorgan/dotfiles
